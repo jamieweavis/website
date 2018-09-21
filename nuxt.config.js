f15cfecb104158a7d8b9377cb5919747175b4cb2
Jamie Weavis <jamie.weavis@gmail.com>
2018-09-21 13:41:37 +0100

    Convert double quotes to single quotes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,47 +1,66 @@
 module.exports = {
   head: {
-    title: "Jamie Weavis - Software Developer",
+    title: 'Jamie Weavis - Software Developer',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "Nuxt.js project" }
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css?family=Oswald|Roboto+Slab|Montserrat"
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png'
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Oswald|Roboto|Montserrat|Lato'
       }
     ]
   },
-  loading: { color: "#f5da55" },
+  loading: { color: '#f5da55' },
   build: {
     extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: "pre",
+          enforce: 'pre',
           test: /\.(js|vue)$/,
-          loader: "eslint-loader",
+          loader: 'eslint-loader',
           exclude: /(node_modules)/
         });
       }
     }
   },
   css: [
-    { src: "@/assets/scss/transitions.scss", lang: "scss" },
-    { src: "@/assets/scss/main.scss", lang: "scss" }
+    { src: '@/assets/scss/transitions.scss', lang: 'scss' },
+    { src: '@/assets/scss/main.scss', lang: 'scss' }
   ],
   modules: ['qonfucius-nuxt-fontawesome'],
   fontAwesome: {
     packs: [
       {
         package: '@fortawesome/fontawesome-free-brands',
-        icons: ['faGithub', 'faTwitter', 'faLinkedinIn'],
+        icons: ['faGithub', 'faTwitter', 'faLinkedinIn']
       },
       {
         package: '@fortawesome/fontawesome-free-solid',
-        icons: ['faHome', 'faAddressCard', 'faBriefcase', 'faFolderOpen', 'faEnvelope'],
-      },
-    ],
+        icons: [
+          'faHome',
+          'faAddressCard',
+          'faBriefcase',
+          'faFolderOpen',
+          'faEnvelope',
+          'faGraduationCap'
+        ]
+      }
+    ]
   }
 };
