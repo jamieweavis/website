093d505f390d5b0d789cb76eca612f1e43487320
Jamie Weavis <jamie.weavis@gmail.com>
2019-03-25 19:31:16 +0000

    Update manifest plugin theme colour

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#171717`,
+        theme_color: `#fd5c63`,
         display: `minimal-ui`,
         icon: `src/images/wave-emoji.png`, // This path is relative to the root of the site.
       },
